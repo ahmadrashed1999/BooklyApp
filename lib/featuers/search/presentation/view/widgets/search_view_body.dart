210e19520fa9feeda03a6841a98a0db29209938b
ahmadrashed1999 <ahmadrashed240@gmail.com>
2023-07-02 19:17:19 +0300

    Done search view ui

--- lib/featuers/search/presentation/view/widgets/search_view_body.dart
+++ lib/featuers/search/presentation/view/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/styles.dart';
+import '../../../../home/presentation/view/widgets/best_seller_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -10,10 +12,42 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
           CustomSearchTextField(),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Expanded(
+            child: SearchResultListView(),
+          )
         ],
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.only(bottom: 20),
+          child: BestSellerListViewItem(),
+        );
+      },
+      itemCount: 10,
     );
   }
 }
