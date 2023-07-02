bfb9d803305b48ea42a8ee1f1e726f0bc6447ca1
ahmadrashed1999 <ahmadrashed240@gmail.com>
2023-07-02 18:43:51 +0300

    add custom scroll view

--- lib/featuers/home/presentation/view/widgets/book_deatils_view_body.dart
+++ lib/featuers/home/presentation/view/widgets/book_deatils_view_body.dart
@@ -14,60 +14,66 @@ class BookDeatilsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: ListView(
-        children: [
-          const CustomBookDeatilsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
-            child: const CustomListViewItem(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          const Text('The Jungle book', style: Styles.textStyle30),
-          const SizedBox(
-            height: 5,
-          ),
-          Opacity(
-            opacity: 0.7,
-            child: Text('The Jungle book',
-                style: Styles.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BookAction(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.w900,
+    return CustomScrollView(slivers: [
+      SliverFillRemaining(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: ListView(
+            children: [
+              const CustomBookDeatilsAppBar(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+                child: const CustomListViewItem(),
               ),
-            ),
+              const SizedBox(
+                height: 43,
+              ),
+              const Text('The Jungle book', style: Styles.textStyle30),
+              const SizedBox(
+                height: 5,
+              ),
+              Opacity(
+                opacity: 0.7,
+                child: Text('The Jungle book',
+                    style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500)),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(
+                height: 37,
+              ),
+              const BookAction(),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'You can also like',
+                  style: Styles.textStyle14.copyWith(
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const SimilarBooksListView(),
+              const SizedBox(
+                height: 40,
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 16,
-          ),
-          const SimilarBooksListView(),
-          const SizedBox(
-            height: 40,
-          ),
-        ],
-      ),
-    );
+        ),
+      )
+    ]);
   }
 }
-
