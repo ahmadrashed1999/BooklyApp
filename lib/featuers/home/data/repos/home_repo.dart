c9dc7a6b13896e2e16e505d03263dd4264f986e7
ahmadrashed1999 <ahmadrashed240@gmail.com>
2023-07-03 14:02:12 +0300

    Create Home repo Method

--- lib/featuers/home/data/repos/home_repo.dart
+++ lib/featuers/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failuers.dart';
+import '../model/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeatuerdBooks();
+  Future<Either<Failuers,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failuers, List<BookModel>>> fetchFeatuerdBooks();
 }
