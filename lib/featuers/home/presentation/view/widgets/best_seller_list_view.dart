108b7364755e84d556067137f974e2e408d36d5a
ahmadrashed1999 <ahmadrashed240@gmail.com>
2023-07-05 00:33:36 +0300

    Cached network image

--- lib/featuers/home/presentation/view/widgets/best_seller_list_view.dart
+++ lib/featuers/home/presentation/view/widgets/best_seller_list_view.dart
@@ -1,5 +1,9 @@
+import 'package:booklyapp/featuers/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/widgets/custom_circualr_indicator.dart';
+import '../../../../../core/widgets/custom_error_widget.dart';
 import 'best_seller_list_view_item.dart';
 
 class BestSellerListView extends StatelessWidget {
@@ -7,16 +11,34 @@ class BestSellerListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.only(bottom: 20),
-          child: BestSellerListViewItem(),
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (
+        context,
+        state,
+      ) {
+        if (state is NewestBooksSuccess) {
+          return ListView.builder(
+            itemCount: state.books.length,
+            physics: const NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: BestSellerListViewItem(),
+              );
+            },
+          );
+        } else if (state is NewestBooksFailuer) {
+          return Center(
+            child: CustomErrorWidget(
+              errMessage: state.message,
+            ),
+          );
+        }
+        return const Center(
+          child: CustomLoadingIndicator(),
         );
       },
-      itemCount: 10,
     );
   }
 }
--- lib/featuers/home/presentation/view/widgets/custom_list_view_item.dart
+++ lib/featuers/home/presentation/view/widgets/custom_list_view_item.dart
@@ -1,19 +1,24 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/widgets/custom_circualr_indicator.dart';
 
 class CustomListViewItem extends StatelessWidget {
   const CustomListViewItem({super.key, required this.imageUrl});
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.8 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: AspectRatio(
+        aspectRatio: 2.8 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          placeholder: (context, url) => const Center(
+            child: CustomLoadingIndicator(),
           ),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
       ),
     );
